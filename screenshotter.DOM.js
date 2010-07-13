5ae77019ea2647446d793e34b289be88f6afeb11
Folletto <folletto@gmail.com>
2010-07-14 00:07:19 +0100

    Added name and timestamp to file name.
    NOT YET WORKING.

--- screenshotter.DOM.js
+++ screenshotter.DOM.js
@@ -35,8 +35,14 @@
   
   // 5
   function screenshotReturn(shared) {
+    function pad2(str) { if ((str + "").length == 1) return "0" + str; return "" + str; }
+    
+    var d = new Date();
+    var timestamp = '' + d.getFullYear() + '' + pad2(d.getMonth() + 1) + '' + pad2(d.getDay()) + '-' + pad2(d.getHours()) + '' + pad2(d.getMinutes()) + '';
+    var filename = "pageshot of '" + shared.tab.title + "' @ " + timestamp;
+    
     var div = window.document.createElement('div');
-    div.innerHTML = '<p style="-webkit-box-shadow: 0px 5px 10px #000000; margin: 20px; background: #ffffff; position: absolute; top: 0; right: 0; z-index: 666; "><img src="' +  shared.imageDataURL + '" width= "400" /></p>';
+    div.innerHTML = '<p style="-webkit-box-shadow: 0px 5px 10px #000000; margin: 20px; background: #ffffff; position: absolute; top: 0; right: 0; z-index: 666; "><img alt="' + filename + '" src="' +  shared.imageDataURL + '" width= "400" /></p>';
     window.document.body.appendChild(div);
   }
   
@@ -49,4 +55,13 @@
         case "screenshotReturn": screenshotReturn(e.shared); break;
       }
   });
+  
+  
+  
+  function makeTimestamp() {
+    var d = new Date();
+    
+    
+    return timestamp;
+  }
 })();
