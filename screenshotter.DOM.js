9248f79d2089a4da8a61be7bf0826030892bcb43
Folletto <folletto@gmail.com>
2018-08-29 16:08:31 +0100

    Fixed "day" in filename.
    Fixes #19.

--- screenshotter.DOM.js
+++ screenshotter.DOM.js
@@ -86,7 +86,7 @@
     function pad2(str) { if ((str + "").length == 1) return "0" + str; return "" + str; }
 
     var d = new Date();
-    var timestamp = '' + d.getFullYear() + '-' + pad2(d.getMonth() + 1) + '-' + pad2(d.getDay()) + '-' + pad2(d.getHours()) + '' + pad2(d.getMinutes()) + '\'' + pad2(d.getSeconds()) + '';
+    var timestamp = '' + d.getFullYear() + '-' + pad2(d.getMonth() + 1) + '-' + pad2(d.getDate()) + '-' + pad2(d.getHours()) + '' + pad2(d.getMinutes()) + '\'' + pad2(d.getSeconds()) + '';
     var filename = "pageshot of '" + normalizeFileName(shared.tab.title) + "' @ " + timestamp;
     var blobURL = dataToBlobURL(shared.imageDataURL);
 
